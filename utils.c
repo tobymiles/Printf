3a3a3dc599896e2ebfbc95fae1a154aa058ed905
S-ergon <sergontoo99@gmail.com>
2023-01-28 06:29:54 -0800

    more changes

--- utils.c
+++ utils.c
@@ -6,7 +6,6 @@
  *
  * Return: 1 if c is printable, 0 otherwise
  */
-
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
@@ -15,7 +14,6 @@ int is_printable(char c)
 	return (0);
 }
 
-
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
@@ -23,7 +21,6 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
@@ -32,9 +29,6 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 		ascii_code *= -1;
 
 	buffer[i++] = '\\';
-	buffer[i++] = 'x';
-
-	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
 
 	return (3);
@@ -46,13 +40,29 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
-
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
-				return (1);
+		return (1);
 
-		return (0);
+	return (0);
+}
+
+/**
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
 }
 
 /**
@@ -62,7 +72,6 @@ int is_digit(char c)
  *
  * Return: Casted value of num
  */
-
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
